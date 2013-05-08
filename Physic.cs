546d9f91136c73136a569fedc5c34669f582e905
tpham93 <gearhunter@gmx.de>
2013-05-08 18:22:13 +0200

    Added several empty Classes (Physic & Player)

---
+++ Physic.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+
+namespace paintRacer
+{
+    class Physic
+    {
+        public static Vector2 calculateVelocity(Vector2 pos, float speed);
+        public static Vector2 calculateVelocity(bool[,] playerCollisionData, short[,] mapData);
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace paintRacer
+{
+    class Player
+    {
+        private bool[,] collisiondata;
+        private EventManager eventManager;
+        private Vector2 position;
+        private float rotation;
+        private float speed;
+        private Texture2D texture;
+        private Vector2 velocity;
+        private Color color;
+
+
+        public Player(String filename, Color color);
+        public static void Init();
+        public void Update(GameTime gameTime);
+        public void Draw(SpriteBatch spriteBatch);
+
+    }
+}
