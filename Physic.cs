82d6f2104a3b6df6aaddab17b4e6c93ec76f3a07
lbedau <ludwig.bedau@st.ovgu.de>
2013-05-08 19:11:40 +0200

    New function to calculate speed

--- Physic.cs
+++ Physic.cs
@@ -17,19 +17,44 @@ namespace paintRacer
          * speed - Vector2 :
          * speed of the car
          * 
-         * acceleration - Vector2 :
-         * acceleration of the car
+         * returns :
+         * new Vector2
          */
-        public static Vector2 calculateNextPos(Vector2 pos, Vector2 speed, Vector2 acceloration);
+        public static Vector2 calculateNextPos(Vector2 pos, Vector2 speed)
+        {
+            return new Vector2(pos.X + speed.X, pos.Y + speed.Y);
+        }
+
+        /*returns the updated speed of the car
+         * 
+         * speed - Vector2 :
+         * speed of the car
+         * 
+         * driverInput - Vector2 :
+         * direction witch is givven by the driver
+         * x: left(-1) strate on(0) right(1)
+         * y: accelorate(1) roll(0) bark(-1)
+         * 
+         * returns :
+         * new Vector2
+         */
+        public static Vector2 calculateSpeed(Vector2 speed, Vector2 dirverInput);
 
         /*returns if there is an collosion
          * 
          * playerCollisionData - bool[,] :
          * defines were in the box are parts of the car
          * 
+         * rotation - float :
+         * defines the rotation of the car
+         * !IN RADIANT!
+         * 
          * mapData - short[,] :
          * defines the type of the Points in the map
+         * 
+         * returns :
+         * bool
          */
-        private static bool hasCollision(bool[,] playerCollisionData, short[,] mapData);
+        private static bool hasCollision(bool[,] playerCollisionData, float rotation, short[,] mapData);
     }
 }
