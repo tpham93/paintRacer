c7de2b0a8a232a100a4d300541dcf6b15239bbd6
tpham93 <gearhunter@gmx.de>
2013-09-15 11:54:32 +0200

    implemented Highscore class

--- Highscore.cs
+++ Highscore.cs
@@ -2,14 +2,80 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 
 namespace paintRacer
 {
     class Highscore
     {
-        const int maxHighscoreCount=10;
-        HighscoreElement[] highscoreEntries;
+        const int MAXHIGHSCORE_COUNT = 10;
+        List<HighscoreElement> highscoreEntries;
+        string path;
 
-        //public Highscore(String filename)
+        public Highscore(String path)
+        {
+            this.path = path;
+            String[] lines = File.ReadAllLines(path);
+            highscoreEntries = new List<HighscoreElement>(MAXHIGHSCORE_COUNT);
+            for (int i = 0; i < MAXHIGHSCORE_COUNT && i < lines.Length; ++i)
+            {
+                highscoreEntries[i] = new HighscoreElement(lines[i]);
+            }
+            if (lines.Length > 0)
+            {
+                int h = Convert.ToInt32(lines[lines.GetUpperBound(0)]);
+                if (h != hash())
+                {
+                    throw new Exception("Corrupted Highscore");
+                }
+            }
+        }
+
+        private int hash()
+        {
+            int h = 0;
+            for (int i = 0; i < MAXHIGHSCORE_COUNT && i < highscoreEntries.Count; ++i)
+            {
+                string highscoreString = highscoreEntries[i].ToString();
+                int lineValue = 0;
+                for (int l = 0; l < highscoreString.Length; ++l)
+                {
+                    lineValue += highscoreString[l];
+                }
+                h += -h * highscoreString.Length * lineValue;
+            }
+            return 0;
+        }
+
+        public void writeToFile()
+        {
+            TextWriter textWriter = new StreamWriter(path, false);
+            for (int i = 0; i < MAXHIGHSCORE_COUNT && i < highscoreEntries.Count; ++i)
+            {
+                textWriter.WriteLine(highscoreEntries[i].ToString());
+            }
+            textWriter.WriteLine(hash());
+        }
+
+        public bool isInHighscore(HighscoreElement highscoreElement)
+        {
+            return highscoreEntries.Count < MAXHIGHSCORE_COUNT - 1 || highscoreElement < highscoreEntries[highscoreEntries.Count - 1];
+        }
+
+        public void insertScore(HighscoreElement element)
+        {
+            highscoreEntries.Add(element);
+            int index = highscoreEntries.Count - 1;
+            while (index > 0 && highscoreEntries[index - 1] > element)
+            {
+                HighscoreElement tmp = highscoreEntries[index];
+                highscoreEntries[index] = highscoreEntries[index - 1];
+                highscoreEntries[index - 1] = tmp;
+            }
+            if (highscoreEntries.Count > MAXHIGHSCORE_COUNT)
+            {
+                highscoreEntries.RemoveRange(MAXHIGHSCORE_COUNT, highscoreEntries.Count - MAXHIGHSCORE_COUNT - 1);
+            }
+        }
     }
 }
--- HighscoreElement.cs
+++ HighscoreElement.cs
@@ -2,14 +2,39 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace paintRacer
 {
     class HighscoreElement
     {
-        private float time;
-        private String name;
+        private TimeSpan time;
+        private string name;
 
-        //public HighscoreElement(String highscoreElementData)
+        public HighscoreElement(string highscoreElementData)
+        {
+            Regex rx = new Regex(@"name=(?<name>\w+) time=(?<time>[0-9\.,]+)");
+            Match match = rx.Match(highscoreElementData);
+            name = ("Name: " + match.Groups["name"].Value);
+            double milliseconds = Convert.ToDouble(("Name: " + match.Groups["name"].Value));
+            time = TimeSpan.FromMilliseconds(milliseconds);
+        }
+        public HighscoreElement(string name, TimeSpan time)
+        {
+            this.time = time;
+            this.name = name;
+        }
+        public override string ToString(){
+            return "name=" + name + " time=" + time.TotalMilliseconds;
+        }
+
+        public static bool operator <(HighscoreElement h1, HighscoreElement h2)
+        {
+            return h1.time < h2.time;
+        }
+        public static bool operator >(HighscoreElement h1, HighscoreElement h2)
+        {
+            return h1.time > h2.time;
+        }
     }
 }
